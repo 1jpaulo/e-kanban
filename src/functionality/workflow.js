e23930f30071e109f711e1336c5b776f5f8faa5c
João Paulo Alvarenga <joao1310al@gmail.com>
2020-05-06 03:02:29 -0300

    Updates severally workflow object, now factory method createTask will be invoked whenever updateUI is invoked, so both state and UI are update at the same pace.

--- src/functionality/workflow.js
+++ src/functionality/workflow.js
@@ -7,53 +7,79 @@
  * This code maintain 
  */ 
 
-// workflow entire object
+/* 
+    workflow object will handle the creation of new tasks 
+    through its factory method createTask
+ */
 function wfobj(){}
+
 wfobj.prototype = {
+
+    createTask: ({title, description}) => ({
+        title,
+        description,
+        timestamp: new Date()
+    }),
+
     todo: {
-        // number of tasks in todo board
         ntask: 0,
-        // union of all those tasks created in todo object
         tasks: []
     },
 
     doing: {
-        // number of tasks in doing board
         ntask: 0,
-        // union of all those tasks created in doing object
         tasks: []
     },
 
     done: {
-        // number of tasks in done board
         ntask: 0,
-        // union of all those tasks created in done object
         tasks: []
     },
-    // this method will be called whenever a task be created
-    // updating the respective board where that task was created
+    // this method will be called whenever a task is created
+    // updating its origin board
     updateUI: function(board)
     {
         var node = document.createElement("DIV");
         node.classList.add("note");
-        this.modalWindow();
-        var text = document.createTextNode(test_input[Math.floor((Math.random() * test_input.length))]);
-        node.appendChild(text);
+        //this.modalWindow();
+        var title = document.createTextNode(test_title[Math.floor((Math.random() * test_title.length))]);
+        var description = document.createTextNode(test_description[Math.floor((Math.random() * test_description.length))]);
+        var task = this.createTask({title, description});
+        node.appendChild(title);
+        node.appendChild(description);
 
         if(board === 'todo')
         {
             let todo = document.querySelectorAll(".board")[0];
+            
+            // update UI
             todo.appendChild(node);
+            
+            // computate state 
+            this.todo.ntask++;
+            this.todo.tasks.push(task);
+        
         }else if(board === 'doing')
         {
             let doing = document.querySelectorAll(".board")[1];
-            doing.appendChild(node);
 
-        }else if(board === 'done'){
-            let done = document.querySelectorAll(".board")[2];
-            done.appendChild(node)
+            // update UI
+            doing.appendChild(node);
+            
+            // computate state 
+            this.doing.ntask++;
+            this.doing.tasks.push(task);
+
         }else{
-            return NULL;
+
+            let done = document.querySelectorAll(".board")[2];
+            // update UI
+            done.appendChild(node);
+            
+            // computate state 
+            this.done.ntask++;
+            this.done.tasks.push(task);
+            
         }
 
         return 1;
@@ -84,47 +110,39 @@ wfobj.prototype = {
     }
 };
 
-var test_input = [
-    'Project Documentation', 'Delete module b from /src', 'Debug Code', 
-    'Recode hash function', 'Undo changes to working tree at commit 08b7d', 
-    'Stretch window board in e-kanban', 'Search a better name for project', 
-    'Writing Documentation', 'Insert Vertical Flexbox', 'Use align content', 
-    'Configure http requests', 'Created subrotine resembling fibonacci function', 
-    'Reduced noise in singularity function', 'Developing a new footer', 
-    'Add copyright terms to footer', 'Add contacts info at the page', 
-    'Publish page at github pages'
-    ]
+var test_title = ['Fix issue #1 from XYZ', 'Fix issue #2 from XYZ', 'Fix issue #3 from XYZ']
+var test_description = ['Try to fix issue #1 from XYZ', 'Try to fix issue #2 from XYZ', 'Try to fix issue #3 from XYZ']
+
 
 function main(){
 
     // instancing workflow object
     var i_wfobj = new wfobj();
     // binding buttons
-    var addtodo = document.querySelectorAll(".tools")[0];
-    var adddoing = document.querySelectorAll(".tools")[1];
-    var adddone = document.querySelectorAll(".tools")[2];
+    var addTodo = document.querySelectorAll(".tools")[0];
+    var addDoing = document.querySelectorAll(".tools")[1];
+    var addDone = document.querySelectorAll(".tools")[2];
     
-    /*addtodo.addEventListener("click", 
+    addTodo.addEventListener("click", 
         function(event){
             //instance of wfobj
             i_wfobj.updateUI('todo');
         }
     );
 
-    adddoing.addEventListener("click", 
+    addDoing.addEventListener("click", 
         function(event){
             //instance of wfobj
             i_wfobj.updateUI('doing');
         }
     );
 
-    adddone.addEventListener("click", 
+    addDone.addEventListener("click", 
         function(event){
             //instance of wfobj
             i_wfobj.updateUI('done');
         }
-    );*/
+    );
 }
 
 window.onload = main;
-
